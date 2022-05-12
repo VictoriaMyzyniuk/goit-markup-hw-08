ce373f2ab5dc127395c98f7b031a9669204b8d33
Victoria Myzynyuk <mezochka93@gmail.com>
2022-05-12 22:22:34 +0300

    fix mistakes after check

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -5,7 +5,9 @@
     menuBtnRef.addEventListener("click", () => {
         const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
         menuBtnRef.classList.toggle("is-open");
+        document.body.classList.toggle("menu-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
         mobileMenuRef.classList.toggle("is-open");
     });
+    
 })();
